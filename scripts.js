c93bfd9565fdb29793e96ce26ec772ac331cfe60
matt22881 <matt22881@gmail.com>
2020-10-25 09:09:14 -0400

    get it working

--- scripts.js
+++ scripts.js
@@ -1,22 +1,44 @@
 const socket = io('http://localhost:5555')
+// const colors = ['mdntgrn','silvjer','silvhel','blk','char','vintgrn','vintsilv','vintgry','vintyel','vintbrn','vintblk']
+
+// const getColor = () => {
+//     const randnum = parseInt(Math.round(Math.random()*11))
+//     return colors[randnum]
+// }
+// const styles = []
 
 const messageContainer = document.getElementById('message-container')
 const messageForm = document.getElementById('send-form')
 const messageInput = document.getElementById('message-input')
 
+let name = prompt('enter a username')
+appendMessage(`You joined as ${name}`, 'user')
+socket.emit('new-user', name)
+   
 socket.on('chat-message', (data) => {
-    appendMessage(data)
+   appendMessage(`${data.name}: ${data.message}`, data.style) 
+})
+
+socket.on('user-connected', (name, style) => {
+    appendMessage(`${name} connected`, style)
+})
+
+socket.on('user-disconnected', (name, style) => {
+    appendMessage(`${name} disconnected`, style)
 })
 
 messageForm.addEventListener('submit', e => {
     e.preventDefault()
     const message = messageInput.value
     socket.emit('send-chat-message', message)
+    appendMessage(`You: ${message}`, 'user')
     messageInput.value = ''
 })
 
-function appendMessage(message) {
+function appendMessage(message, style) {
     const messageElement = document.createElement('div')
+    messageElement.classList.add(style)
+    messageElement.classList.add('message')
     messageElement.innerText = message
-    messageContainer.append(messageElement)
+    messageContainer.insertBefore(messageElement, messageContainer.firstChild)
 }
--- server.js
+++ server.js
@@ -1,9 +1,42 @@
 const io = require('socket.io')(5555)
 
+const colors = ['mdntgrn','silvjer','silvhel','blk','char','vintgrn','vintsilv','vintgry','vintyel','vintbrn','vintblk']
+
+const getColor = () => {
+    const randnum = Math.round(Math.random()*11)
+    return colors[randnum]
+}
+
+const styles = {}
+const users = {}
+
 io.on('connection', (socket) => {
-    console.log('new-user')
+    
+    socket.on('new-user', name => {
+        const style = getColor()
+        users[socket.id] = name
+        styles[socket.id] = style
+        socket.broadcast.emit('user-connected', name, style)
+        console.log(name ,' connected')
+        console.log(users)
+        console.log(styles)
+    })
+
     socket.on('send-chat-message', message => {
-        socket.broadcast.emit('chat-message', message)
-        console.log('server-message')
+        socket.broadcast.emit('chat-message', {
+            message: message,
+            name: users[socket.id],
+            style: styles[socket.id]
+        })
+   })
+
+   socket.on('disconnect', () => {
+       socket.broadcast.emit('user-disconnected', users[socket.id], styles[socket.id])
+       console.log(users[socket.id] ,' disconnected')
+       delete users[socket.id]
+       delete styles[socket.id]
+    console.log(users)
+    console.log(styles)
    })
 })
+  
